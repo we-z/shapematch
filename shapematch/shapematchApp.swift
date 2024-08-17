6532b87c3de2ea84348fc1c46a5ba12c1c243898
we-z <yousefsalem00@gmail.com>
2024-08-17 13:20:19 -0700

    Initial Commit

---
+++ shapematch/shapematchApp.swift
@@ -0,0 +1,17 @@
+//
+//  shapematchApp.swift
+//  shapematch
+//
+//  Created by Wheezy Capowdis on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct shapematchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
