b6e67e30400c52b8702d67623cd5d4d1daa3ebc3
we-z <yousefsalem00@gmail.com>
2024-08-22 21:34:47 -0700

    place holder menu

--- shapematch/GemMenuView.swift
+++ shapematch/GemMenuView.swift
@@ -12,14 +12,142 @@ struct GemMenuView: View {
         VStack {
             HStack{
                 Spacer()
-                Text("💎 Gems 💎")
-                    .bold()
-                    .font(.system(size: deviceWidth/8))
+                Text("❌")
+                    .font(.system(size: deviceWidth/15))
                     .customTextStroke()
-                    .padding(30)
-                Spacer()
+                    .padding([.trailing, .top], 21)
             }
-            Spacer()
+            Text("💎 Gems 💎")
+                .bold()
+                .font(.system(size: deviceWidth/8))
+                .customTextStroke()
+                
+//            Spacer()
+            VStack(spacing: 21) {
+                Button {
+                } label: {
+                    HStack{
+                        Text("💎")
+                            .bold()
+                            .italic()
+                            .font(.system(size: deviceWidth/15))
+                            .fixedSize()
+                            .scaleEffect(1.5)
+                            .customTextStroke()
+                            .padding(12)
+                        Text("10")
+                            .bold()
+                            .font(.system(size: deviceWidth/12))
+                            .customTextStroke(width: 1.5)
+                        Spacer()
+                        Text("$1.99")
+                            .font(.system(size: deviceWidth/21))
+                            .customTextStroke(width:1.2)
+                            .padding(9)
+                            .background(.green)
+                            .cornerRadius(15)
+                            .overlay{
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(Color.black, lineWidth: 3)
+                                    .padding(1)
+                            }
+                            .padding(.trailing, 3)
+                    }
+                    .padding()
+                    .background(.blue)
+                    .cornerRadius(15)
+                    .overlay{
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(Color.black, lineWidth: 4)
+                            .padding(1)
+                    }
+                    .padding(.horizontal, 30)
+                }
+                .buttonStyle(.roundedAndShadow6)
+                Button {
+                } label: {
+                    HStack{
+                        Text("💎")
+                            .bold()
+                            .italic()
+                            .font(.system(size: deviceWidth/15))
+                            .fixedSize()
+                            .scaleEffect(1.5)
+                            .customTextStroke()
+                            .padding(12)
+                        Text("100")
+                            .bold()
+                            .font(.system(size: deviceWidth/12))
+                            .customTextStroke(width: 1.5)
+                            
+                        Spacer()
+                        Text("$14.99")
+                            .font(.system(size: deviceWidth/21))
+                            .customTextStroke(width:1.2)
+                            .padding(9)
+                            .background(.green)
+                            .cornerRadius(15)
+                            .overlay{
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(Color.black, lineWidth: 3)
+                                    .padding(1)
+                            }
+                            .padding(.trailing, 3)
+                    }
+                    .padding()
+                    .background(.blue)
+                    .cornerRadius(15)
+                    .overlay{
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(Color.black, lineWidth: 4)
+                            .padding(1)
+                    }
+                    .padding(.horizontal, 30)
+                }
+                .buttonStyle(.roundedAndShadow6)
+                Button {
+                } label: {
+                    HStack{
+                        Text("💎")
+                            .bold()
+                            .italic()
+                            .font(.system(size: deviceWidth/15))
+                            .fixedSize()
+                            .scaleEffect(1.5)
+                            .customTextStroke()
+                            .padding(12)
+                        Text("1000")
+                            .bold()
+                            .font(.system(size: deviceWidth/12))
+                            .customTextStroke(width: 1.5)
+
+                        Spacer()
+                        Text("$99.99")
+                            .font(.system(size: deviceWidth/21))
+                            .customTextStroke(width:1.2)
+                            .padding(9)
+                            .background(.green)
+                            .cornerRadius(15)
+                            .overlay{
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(Color.black, lineWidth: 3)
+                                    .padding(1)
+                            }
+                            .padding(.trailing, 3)
+                    }
+                    .padding()
+                    .background(.blue)
+                    .cornerRadius(15)
+                    .overlay{
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(Color.black, lineWidth: 4)
+                            .padding(1)
+                    }
+                    .padding(.horizontal, 30)
+                }
+                .buttonStyle(.roundedAndShadow6)
+            }
+            .padding(.bottom, 30)
         }
         .background(.blue)
         .cornerRadius(30)
