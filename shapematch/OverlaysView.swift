621d5d492c016aad85076f2b12a2c057b9993c55
we-z <yousefsalem00@gmail.com>
2024-08-26 08:40:07 -0700

    instructions

--- shapematch/OverlaysView.swift
+++ shapematch/OverlaysView.swift
@@ -20,7 +20,46 @@ struct OverlaysView: View {
             } else if appModel.showNoMoreSwipesView {
                 NoMoreSwipesView()
             }
+            if appModel.level == 1 {
+                InstructionView()
+            }
             CelebrationEffect()
+        }
+    }
+}
+
+struct InstructionView: View {
+    @State var cardOffset: CGFloat = 0
+    var body: some View {
+        VStack{
+            HStack{
+                Spacer()
+                Text("↘️ Copy The Goal ↙️")
+                    .bold()
+                    .font(.system(size: deviceWidth / 12))
+                    .customTextStroke()
+                Spacer()
+            }
+            .padding()
+            .background(.yellow)
+            .cornerRadius(15)
+            .overlay{
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(Color.black, lineWidth: 6)
+                    .padding(1)
+            }
+            .padding(.horizontal)
+            .offset(y: cardOffset)
+            
+            Spacer()
+        }
+        .onAppear{
+            DispatchQueue.main.async {
+                cardOffset = -(deviceWidth/2)
+                withAnimation(.interpolatingSpring(mass: 2.0, stiffness: 100.0, damping: 10.0, initialVelocity: 0.0)) {
+                    cardOffset = 0
+                }
+            }
         }
     }
 }
