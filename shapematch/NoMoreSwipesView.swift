56bf93f9dba41a8a3f8d6da2d16093d42b1ad125
we-z <yousefsalem00@gmail.com>
2024-09-16 09:23:01 -0700

    revamped loss screen

--- shapematch/NoMoreSwipesView.swift
+++ shapematch/NoMoreSwipesView.swift
@@ -11,10 +11,9 @@ import AVFoundation
 struct NoMoreSwipesView: View {
     
     @ObservedObject private var appModel = AppModel.sharedAppModel
-    @State var buySwapsButtonOffset = deviceWidth/2
-    @State var resetButtonOffset = -(deviceWidth/2)
+    @State var buttonsnOffset = deviceWidth
+    @State var bannerOffset = -(deviceWidth/2)
     @State var pulseText = true
-    @State var textScale: CGFloat = 3
     @ObservedObject var userPersistedData = UserPersistedData.sharedUserPersistedData
     
     func resetGame() {
@@ -33,124 +32,157 @@ struct NoMoreSwipesView: View {
                             if value.translation.height < 0 {
                                 // Swipe up detected
                                 DispatchQueue.main.async { [self] in
-                                    withAnimation(.interpolatingSpring(mass: 3.0, stiffness: 100.0, damping: 9.0, initialVelocity: 0.0)) {
-                                        buySwapsButtonOffset = 0
+                                    withAnimation(.interpolatingSpring(mass: 3.0, stiffness: 100.0, damping: 18.0, initialVelocity: 0.0)) {
+                                        buttonsnOffset = 0
                                     }
                                 }
                             }
                             if value.translation.height > 0 {
-                                // Swipe up detected
+                                // Swipe down detected
                                 DispatchQueue.main.async { [self] in
-                                    withAnimation(.linear) {
-                                        buySwapsButtonOffset = deviceWidth/3
+                                    if bannerOffset == 0 {
+                                        withAnimation(.linear) {
+                                            buttonsnOffset = deviceWidth
+                                        }
+                                    }
+                                    withAnimation(.interpolatingSpring(mass: 3.0, stiffness: 100.0, damping: 18.0, initialVelocity: 0.0)) {
+                                        bannerOffset = 0
                                     }
                                 }
                             }
                         }
                 )
-//            ZStack {
-                FailFireEffect()
-                    .frame(width: deviceWidth)
-                    .offset(y: deviceWidth/15)
-                Text("0 Swaps left ✋")
-                    .bold()
-                    .font(.system(size: deviceWidth/9))
-                    .customTextStroke(width: 2.4)
-                
-                    .allowsHitTesting(false)
-//            }
-            .scaleEffect(textScale)
             VStack{
-                HStack{
+                HStack {
                     Spacer()
-                    Button {
-                        resetGame()
-                        impactHeavy.impactOccurred()
-                    } label: {
-                        Text("🔄")
-                            .font(.system(size: deviceWidth/6))
-                            .customTextStroke(width: 2)
-                            .padding([.trailing], 9)
-                            .offset(y: resetButtonOffset)
-                    }
+                    Text("0 Swaps left ✋")
+                        .bold()
+                        .font(.system(size: deviceWidth/9))
+                        .customTextStroke(width: 2.4)
+                    Spacer()
                 }
-                .buttonStyle(.roundedAndShadow6)
-                Spacer()
-                Button {
-                    if appModel.swapsToSell > userPersistedData.gemBalance {
-                        appModel.showGemMenu = true
-                    } else {
-                        userPersistedData.decrementBalance(amount: appModel.swapsToSell)
-                        appModel.swipesLeft += appModel.swapsToSell
-                        appModel.showNoMoreSwipesView = false
-                    }
-                } label: {
-                    HStack{
-                        Spacer()
-                        Text("💎")
-                            .bold()
-                            .italic()
-                            .font(.system(size: deviceWidth/13))
-                            .fixedSize()
-                            .scaleEffect(1.5)
-                            .customTextStroke(width: 2.4)
-                        
-                        Text("+ \(appModel.swapsToSell) Swaps")
-                            .bold()
-                            .font(.system(size: deviceWidth/10))
-                            .fixedSize()
-                            .customTextStroke(width: 2.4)
-                            .padding(.horizontal, 9)
-                        Text("↔️")
-                            .bold()
-                            .italic()
-                            .font(.system(size: deviceWidth/13))
-                            .fixedSize()
-                            .scaleEffect(1.2)
-                            .customTextStroke(width: 2.4)
-                        Spacer()
-                    }
-                    .padding()
-                    .background(.blue)
-                    .cornerRadius(21)
-                    .overlay{
-                        RoundedRectangle(cornerRadius: 21)
-                            .stroke(Color.black, lineWidth: 5)
-                            .padding(1)
-                    }
-                    .padding(.bottom, 6)
-                    .gesture(
-                        DragGesture(minimumDistance: 1, coordinateSpace: .local)
-                            .onEnded { value in
-                                if value.translation.height > 0 {
-                                    // Swipe up detected
-                                    DispatchQueue.main.async { [self] in
-                                        withAnimation(.linear) {
-                                            buySwapsButtonOffset = deviceWidth/3
-                                        }
+                .padding(12)
+                .background(.red)
+                .cornerRadius(21)
+                .overlay{
+                    RoundedRectangle(cornerRadius: 21)
+                        .stroke(Color.black, lineWidth: 6)
+                        .padding(1)
+                }
+                .offset(y: bannerOffset)
+                .gesture(
+                    DragGesture(minimumDistance: 1, coordinateSpace: .local)
+                        .onEnded { value in
+                            if value.translation.height < 0 {
+                                // Swipe up detected
+                                DispatchQueue.main.async { [self] in
+                                    withAnimation(.interpolatingSpring(mass: 3.0, stiffness: 100.0, damping: 18.0, initialVelocity: 0.0)) {
+                                        bannerOffset = -(deviceWidth/2)
                                     }
                                 }
                             }
-                    )
+                        }
+                )
+                Spacer()
+                VStack(spacing: 21){
+                    HStack{
+                        Button {
+                            resetGame()
+                            impactHeavy.impactOccurred()
+                        } label: {
+                            Text("🔀")
+                                .font(.system(size: deviceWidth/6))
+                                .customTextStroke(width: 2)
+                                .padding([.trailing], 9)
+                        }
+                        Spacer()
+                        Button {
+                            resetGame()
+                            impactHeavy.impactOccurred()
+                        } label: {
+                            Text("🔄")
+                                .font(.system(size: deviceWidth/6))
+                                .customTextStroke(width: 2)
+                                .padding([.trailing], 9)
+                        }
+                    }
+                    .buttonStyle(.roundedAndShadow6)
+                    Button {
+                        if appModel.swapsToSell > userPersistedData.gemBalance {
+                            appModel.showGemMenu = true
+                        } else {
+                            userPersistedData.decrementBalance(amount: appModel.swapsToSell)
+                            appModel.swipesLeft += appModel.swapsToSell
+                            appModel.showNoMoreSwipesView = false
+                        }
+                    } label: {
+                        HStack{
+                            Spacer()
+                            Text("💎")
+                                .bold()
+                                .italic()
+                                .font(.system(size: deviceWidth/13))
+                                .fixedSize()
+                                .scaleEffect(1.5)
+                                .customTextStroke(width: 2.4)
+                            
+                            Text("+ \(appModel.swapsToSell) Swaps")
+                                .bold()
+                                .font(.system(size: deviceWidth/10))
+                                .fixedSize()
+                                .customTextStroke(width: 2.4)
+                                .padding(.horizontal, 9)
+                            Text("↔️")
+                                .bold()
+                                .italic()
+                                .font(.system(size: deviceWidth/13))
+                                .fixedSize()
+                                .scaleEffect(1.2)
+                                .customTextStroke(width: 2.4)
+                            Spacer()
+                        }
+                        .padding()
+                        .background(.blue)
+                        .cornerRadius(21)
+                        .overlay{
+                            RoundedRectangle(cornerRadius: 21)
+                                .stroke(Color.black, lineWidth: 5)
+                                .padding(1)
+                        }
+                        .padding(.bottom, 21)
+                        
+                    }
+                    .buttonStyle(.roundedAndShadow6)
                 }
-                .buttonStyle(.roundedAndShadow6)
-                .offset(y: buySwapsButtonOffset)
+                .gesture(
+                    DragGesture(minimumDistance: 1, coordinateSpace: .local)
+                        .onEnded { value in
+                            if value.translation.height > 0 {
+                                // Swipe up detected
+                                DispatchQueue.main.async { [self] in
+                                    withAnimation(.linear) {
+                                        buttonsnOffset = deviceWidth
+                                    }
+                                }
+                            }
+                        }
+                )
+                .offset(y: buttonsnOffset)
                 
             }
-            .padding()
+            .padding(.horizontal)
             .background(.clear)
         }
         .onAppear {
             AudioServicesPlaySystemSound (1053)
             DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [self] in
-                withAnimation(.interpolatingSpring(mass: 3.0, stiffness: 100.0, damping: 9.0, initialVelocity: 0.0)) {
-                    buySwapsButtonOffset = 0
-                    resetButtonOffset = 0
+                withAnimation(.interpolatingSpring(mass: 3.0, stiffness: 100.0, damping: 18.0, initialVelocity: 0.0)) {
+                    buttonsnOffset = 0
                 }
             }
-            DispatchQueue.main.async {
-                withAnimation(.interpolatingSpring(mass: 3.0, stiffness: 120.0, damping: 16.0, initialVelocity: 0.0)) {
-                    textScale = 1
+            DispatchQueue.main.async { [self] in
+                withAnimation(.interpolatingSpring(mass: 3.0, stiffness: 100.0, damping: 18.0, initialVelocity: 0.0)) {
+                    bannerOffset = 0
                 }
             }
         }
