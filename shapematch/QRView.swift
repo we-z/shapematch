ad38525068d3bfbba8d700e250e374df51dd3314
we-z <yousefsalem00@gmail.com>
2024-09-17 10:14:56 -0700

    QRView

---
+++ shapematch/QRView.swift
@@ -0,0 +1,37 @@
+//
+//  QRView.swift
+//  Shape Swap
+//
+//  Created by Wheezy Capowdis on 9/17/24.
+//
+
+import SwiftUI
+
+struct QRView: View {
+    var body: some View {
+        ZStack {
+            Color.blue
+            VStack {
+                ZStack{
+                    Rectangle()
+                        .frame(width: 315, height: 315)
+                        .cornerRadius(54)
+                    Image("qrcode")
+                        .resizable()
+                        .frame(width: 300, height: 300)
+                        .cornerRadius(45)
+                }
+                Text("Shape Swap")
+                    .font(.system(size: 45))
+                    .bold()
+                    .customTextStroke(width: 2.7)
+            }
+            .scaleEffect(0.6)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    QRView()
+}
