c3a996b75861382f69ec6e087843edee57510cf9
we-z <yousefsalem00@gmail.com>
2024-09-17 10:02:49 -0700

    more subtle tint

--- shapematch/GemMenuView.swift
+++ shapematch/GemMenuView.swift
@@ -37,7 +37,7 @@ struct GemMenuView: View {
     
     var body: some View {
         ZStack {
-            Color.blue.opacity(0.3)
+            Color.blue.opacity(0.15)
                 .ignoresSafeArea()
                 .onTapGesture {
                     appModel.showGemMenu = false
