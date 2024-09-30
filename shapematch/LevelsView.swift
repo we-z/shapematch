114cf0f551fc2e78cc20717e40acc09b38f09572
we-z <yousefsalem00@gmail.com>
2024-09-30 06:13:52 -0700

    LevelsView file

---
+++ shapematch/LevelsView.swift
@@ -0,0 +1,18 @@
+//
+//  LevelsView.swift
+//  Shape Swap
+//
+//  Created by Wheezy Capowdis on 9/30/24.
+//
+
+import SwiftUI
+
+struct LevelsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LevelsView()
+}
