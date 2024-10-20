a515bb1a6c4ea45ac6716fa0524938672332e2de
we-z <yousefsalem00@gmail.com>
2024-10-20 11:19:30 -0700

    add button and stars to continue view

--- shapematch/LevelCompleteView.swift
+++ shapematch/LevelCompleteView.swift
@@ -15,14 +15,66 @@ struct LevelCompleteView: View {
                 
                 VStack{
                     HStack{
+                        Text("❌")
+                            .bold()
+                            .font(.system(size: deviceWidth / 12))
+                            .customTextStroke(width: 1.5)
+                            .padding(.top)
+                            .opacity(0)
                         Spacer()
                         Text("Level \(userPersistedData.level)")
                             .bold()
                             .font(.system(size: deviceWidth / 9))
-                            .customTextStroke(width: 3)
-                            .padding()
+                            .customTextStroke(width: 2.1)
+                            .padding(.top)
                         Spacer()
+                        Text("❌")
+                            .bold()
+                            .font(.system(size: deviceWidth / 12))
+                            .customTextStroke(width: 1.5)
+                            .padding(.top)
                     }
+                    .padding(.horizontal, 30)
+//                    Text("🥳")
+//                        .font(.system(size: deviceWidth / 3))
+//                        .customTextStroke(width: 3)
+//                        .padding(.bottom, 1)
+                    HStack{
+                        Text("⭐️")
+                        Text("⭐️")
+                            .padding(.horizontal)
+                        Text("⭐️")
+                    }
+                    .font(.system(size: deviceWidth / 6))
+                    .customTextStroke(width: 2.7)
+                    .padding(1)
+                    Button {
+
+                    } label: {
+                        HStack{
+                            Spacer()
+                            Text("Continue  ➡️")
+                                .italic()
+                                .bold()
+                                .font(.system(size: deviceWidth/12))
+                                .fixedSize()
+                                .customTextStroke(width: 1.8)
+                            Spacer()
+                        }
+                        .padding()
+                        .background{
+                            LinearGradient(gradient: Gradient(colors: [.green, .green]), startPoint: UnitPoint(x: 0.5, y: 0), endPoint: UnitPoint(x: 0.5, y: 0.3))
+                        }
+                        .cornerRadius(21)
+                        .overlay{
+                            RoundedRectangle(cornerRadius: 21)
+                                .stroke(Color.black, lineWidth: idiom == .pad ? 9 : 5)
+                                .padding(1)
+                        }
+                        .padding(30)
+                        
+                    }
+                    .buttonStyle(.roundedAndShadow6)
                 }
                 .background{
                     Color.white
